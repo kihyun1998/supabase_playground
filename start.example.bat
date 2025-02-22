a8a75ba0667604739de70ebfecf86c4d4a07b158
박기현 <kihyun1998@penta.co.kr>
2025-02-22 21:46:06 +0900

    Update start.example.bat

--- start.example.bat
+++ start.example.bat
@@ -1,3 +1,3 @@
 @echo off
-flutter run --dart-define=SUPABASE_KEY=your_supabase_key
+flutter run --dart-define=SUPABASE_URL=your_supabase_url --dart-define=SUPABASE_KEY=your_supabase_key --dart-define=GCP_WEB_CLIENT_ID=your_gcp_web_client_id  --dart-define=GCP_ANDROID_CLIENT_ID=your_gcp_android_client_id
 pause
